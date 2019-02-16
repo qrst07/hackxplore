1d328e0d8943c9076b1a4712704884e355e93401
Anusha Dey <aunosua00@gmail.com>
2019-02-16 12:24:55 -0500

    general file for detecting specific colour

---
+++ detect_colors/detect_color1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import argparse
+import cv2
+ 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+# color list of boundaries
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+ 
+	mask = cv2.inRange(image, lower, upper)
+	output = cv2.bitwise_and(image, image, mask = mask)
+ 
+#uncomment following line if you want to see the image
+	# cv2.imshow("images", np.hstack([image, output]))
+
+#the following lines will write a file with the colours outlined above
+	cv2.imwrite( "./Test_Image.jpg", np.hstack([output]))
+	cv2.waitKey(0)
